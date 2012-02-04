7e031accc67e0a2d3da377ee881fba838dc02bd9
Samuel Harlan <harlan.samuel@gmail.com>
2012-02-03 20:56:25 -0500

    revision i fucked it up

--- src/question3.java
+++ src/question3.java
@@ -4,13 +4,13 @@ import java.lang.StringBuilder;
 
 
 public class question3 {
-	public static String byteArrayToString(byte [] array)
+    public static String byteArrayToString(byte [] array)
     {
-		return new String(array);
+        return new String(array);
     }
-	public static void printByteArray(byte [] array)
+    public static void printByteArray(byte [] array)
     {
-		System.out.print("Plain Text Hex Format: ");
+        System.out.print("Plain Text Hex Format: ");
         System.out.print("[");
         for(int i = 0; i < array.length-1; i++)
         {
@@ -19,66 +19,66 @@ public class question3 {
         }
         System.out.print(Integer.toHexString(array[array.length-1]>>4&0x0F).toUpperCase());
         System.out.println(Integer.toHexString(array[array.length-1]&0x0F).toUpperCase() + "]");
-        String str = new String(array); 
+        String str = new String(array);
         System.out.println("Plain Text Readable: "+str);
     }
 
-	public static void main (String [] args0)
-	{
-		Cipher cipher;
-		SecretKeySpec key;
-		
-		byte [] keyBytes;
-		byte [] pt;
-		byte [] ct;
-		
-		keyBytes = new byte [] {(byte) 0x01, (byte) 0x23, (byte) 0x45, (byte) 0x67, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
-		ct = new byte [] {(byte) 0x20, (byte) 0xAD, (byte) 0xFD, (byte) 0xBF, (byte) 0x1B, (byte) 0xB6, (byte) 0x9F, (byte) 0x4B};
-		int counter = 1;
-		for(int i = 0; i < 256; i++)
-		{
-			counter = 1;
-			System.out.println("Bit 5 incremented");
-			for (int j = 0; j < 256; j++)
-			{ 
-				System.out.println(counter);
-				//printByteArray(keyBytes);
-				
-				
-				for (int k = 0; k<256; k++)
-				{
-					for (int l = 0; l <256; l++)
-					{
-						key = new SecretKeySpec(keyBytes, "DES");
-						try
-						{
-							cipher = Cipher.getInstance("DES/ECB/NoPadding");
-							cipher.init(Cipher.DECRYPT_MODE, key);
-							pt = cipher.doFinal(ct);
-							
-							//printByteArray(pt);
-							if (new String(pt).equals("Plaintxt"))
-							{
-								printByteArray(keyBytes);
-								System.out.println("answer");
-							}
-							//printByteArray(keyBytes);
-							
-						}
-						catch(Exception e)
-						{
-							e.printStackTrace();
-						}
-						keyBytes[7] = (byte) (keyBytes[7] + 1);
-					}
-					keyBytes[6] = (byte) (keyBytes[6] + 1);
-				}
-				keyBytes[5] = (byte) (keyBytes[5] + 1);
-				counter++;
-			}
-			keyBytes[4] = (byte) (keyBytes[4] + 1);
-			
-		}
-		
-	}
+    public static void main (String [] args0)
+    {
+        Cipher cipher;
+        SecretKeySpec key;
+       
+        byte [] keyBytes;
+        byte [] pt;
+        byte [] ct;
+       
+        keyBytes = new byte [] {(byte) 0x01, (byte) 0x23, (byte) 0x45, (byte) 0x67, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+        ct = new byte [] {(byte) 0x20, (byte) 0xAD, (byte) 0xFD, (byte) 0xBF, (byte) 0x1B, (byte) 0xB6, (byte) 0x9F, (byte) 0x4B};
+        int counter = 1;
+        for(int i = 0; i < 256; i++)
+        {
+            counter = 1;
+            System.out.println("Bit 5 incremented");
+            for (int j = 0; j < 256; j++)
+            {
+                System.out.println(counter);
+                //printByteArray(keyBytes);
+               
+               
+                for (int k = 0; k<256; k++)
+                {
+                    for (int l = 0; l <256; l++)
+                    {
+                        key = new SecretKeySpec(keyBytes, "DES");
+                        try
+                        {
+                            cipher = Cipher.getInstance("DES/ECB/NoPadding");
+                            cipher.init(Cipher.DECRYPT_MODE, key);
+                            pt = cipher.doFinal(ct);
+                           
+                            //printByteArray(pt);
+                            if (new String(pt).equals("Plaintxt"))
+                            {
+                                printByteArray(keyBytes);
+                                System.out.println("answer");
+                            }
+                            //printByteArray(keyBytes);
+                           
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+                        keyBytes[7] = (byte) (keyBytes[7] + 1);
+                    }
+                    keyBytes[6] = (byte) (keyBytes[6] + 1);
+                }
+                keyBytes[5] = (byte) (keyBytes[5] + 1);
+                counter++;
+            }
+            keyBytes[4] = (byte) (keyBytes[4] + 1);
+           
+        }
+       
+    }
 }
